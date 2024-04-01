65bae0e77c9bd7a69fb9335fab3b611addefffba
WebDiyar <diyar.atashka@mail.ru>
2024-04-01 13:22:11 +0600

    timeout on animation

--- 2.AnimationInput/components/CommentInput .tsx
+++ 2.AnimationInput/components/CommentInput .tsx
@@ -1,16 +1,22 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {Animated, Dimensions, TextInput, TouchableOpacity, View} from 'react-native';
-import Icon from 'react-native-vector-icons/Feather';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import {Entypo} from '@expo/vector-icons';
-import {FontAwesome} from '@expo/vector-icons';
+import React, { useEffect, useRef, useState } from 'react';
+import { Animated, Dimensions, TextInput, TouchableOpacity, View } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function CommentInput() {
     const [value, setValue] = useState<string>('');
-    const {width} = Dimensions.get('window');
+    const { width } = Dimensions.get('window');
 
     const animatedSendScale = useRef(new Animated.Value(0)).current;
     const animatedMicScale = useRef(new Animated.Value(1)).current;
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            animateIcons();
+        }, 100); // Задержка в 100 миллисекунд
+
+        return () => clearTimeout(timer); // Очистка таймера при размонтировании
+    }, [value]);
 
     const animateIcons = () => {
         if (value.trim()) {
@@ -27,11 +33,6 @@ export default function CommentInput() {
             }).start(() => animatedSendScale.setValue(0));
         }
     };
-
-    useEffect(() => {
-        animateIcons();
-    }, [value]);
-
 
     const handleChange = (text: string) => {
         setValue(text);
@@ -62,7 +63,7 @@ export default function CommentInput() {
                 <TextInput
                     value={value}
                     onChangeText={handleChange}
-                    style={{height: '100%', paddingHorizontal: 10, fontSize: 18}}
+                    style={{ height: '100%', paddingHorizontal: 10, fontSize: 18 }}
                     multiline
                     placeholder="Enter message..."
                 />
@@ -82,24 +83,15 @@ export default function CommentInput() {
                     elevation: 2,
                 }}
             >
-
-
-                {/* <Animated.View style={{transform: [{scale: animatedMicScale}]}}>
-                    {value.trim().length > 0 ? (<Entypo name="paper-plane" size={24} color="white"/>
-                    ) : <FontAwesome name="microphone" size={24} color="white"/>
-                    }
-                </Animated.View> */}
-
                 {value.trim().length > 0 ? (
-                    <Animated.View style={{ transform: [{ scale: animatedSendScale }] }} >
+                    <Animated.View style={{ transform: [{ scale: animatedSendScale }] }}>
                         <Entypo name="paper-plane" size={24} color="white" />
                     </Animated.View>
-                ) :
-                   <Animated.View style={{ transform: [{ scale: animatedMicScale }] }}>
+                ) : (
+                    <Animated.View style={{ transform: [{ scale: animatedMicScale }] }}>
                         <FontAwesome name="microphone" size={24} color="white" />
                     </Animated.View>
-                }
-
+                )}
             </TouchableOpacity>
         </View>
     );
